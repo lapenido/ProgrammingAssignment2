39cc030bbd6ab01c77697675ff5b3399ce133b9d
Luiz Augusto Penido <lapenido@yahoo.com.br>
2014-10-26 03:49:30 -0200

    my solution cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+###
+### This two functions calculates inverse matrix and cache it.
+### If the same matrix is use, then the inverse its not re-calculate  
+### and the function return cached matrix.
+###
 
-## Write a short comment describing this function
+## This function transforms a matrix into a data structure. 
+## This data structure is a list that contains a cached matrix 
+## and some methods to manipulate it. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		CacheInverseMatrix <- NULL
+        
+		setMatrix <- function(y){
+                x <<- y
+                CacheInverseMatrix <<- NULL
+        }
+		
+        getMatrix <- function(){ 
+		  x
+		}
+		
+        setInverseMatrix <- function(solve){
+			CacheInverseMatrix <<- solve
+        }
+        
+		getInverseMatrix <- function() {
+			CacheInverseMatrix
+        } 	
+		
+        list( setMatrix = setMatrix
+		     ,getMatrix = getMatrix
+			 ,setInverseMatrix = setInverseMatrix
+			 ,getInverseMatrix = getInverseMatrix 
+			)
 }
 
-
-## Write a short comment describing this function
+## This function receives a data structure that 
+## represents a matrix and its  returns the inverse matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+		InverseMatrix <- x$getInverseMatrix()
+		
+        if(!is.null(InverseMatrix)) {
+            message("getting cached inverse matrix")
+            return(InverseMatrix)
+        }
+        
+		data <- x$getMatrix()
+        
+		InverseMatrix <- solve(data, ...)
+        
+		x$setInverseMatrix(InverseMatrix)
+		
+        InverseMatrix	
 }
